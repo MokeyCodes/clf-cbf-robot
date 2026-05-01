10a1dfc61286e19af301dc68c968d2f7e0c46841
MokeyCodes <enguyenhuu@ucsd.edu>
2026-04-30 18:22:54 -0700

    Untrack ROS2 install artifacts

--- ros2_ws/build/rrt_cbf_planner/share/rrt_cbf_planner/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/turtlebot/ethan_RRT_ws/clf-cbf-robot/ros2_ws/build/rrt_cbf_planner"
